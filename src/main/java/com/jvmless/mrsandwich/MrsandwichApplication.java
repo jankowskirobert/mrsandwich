fd8f52e9ddad22b53bc50584db09c9a95ec333fb
Robert <jankowski.r@hotmail.com>
2018-09-30 21:59:31 +0200

    Empty repo

---
+++ src/main/java/com/jvmless/mrsandwich/MrsandwichApplication.java
@@ -0,0 +1,12 @@
+package com.jvmless.mrsandwich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrsandwichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrsandwichApplication.class, args);
+	}
+}
