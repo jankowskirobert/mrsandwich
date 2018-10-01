833b384ab3c6be6262d3c6d030ccd918a0a8425b
Robert Jankowski <jankowski.r@hotmail.com>
2018-10-01 23:33:05 +0200

    GH-5 concept of seller model

---
+++ src/main/java/com/jvmless/mrsandwich/seller/Seller.java
@@ -0,0 +1,13 @@
+package com.jvmless.mrsandwich.seller;
+
+import java.util.List;
+
+public class Seller {
+    private String id;
+    private PersonalData personalData;
+    private Area area;
+    private List<Location> locations;
+    public static Seller by(SellerRegisterRequestDto dto) {
+        return null;
+    }
+}
---
+++ src/main/java/com/jvmless/mrsandwich/seller/SellerRegisterRequestDto.java
@@ -0,0 +1,13 @@
+package com.jvmless.mrsandwich.seller;
+
+import java.util.List;
+
+public class SellerRegisterRequestDto {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String postalCode;
+    private String schedulerId;
+    private List<String> locations;
+}
