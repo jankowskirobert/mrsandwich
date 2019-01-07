95d96aeaa24c13bcdd3acbaf1881c5a34ffc3fb2
Robert Jankowski <jankowski.r@hotmail.com>
2019-01-07 01:49:52 +0100

    Location runnable

--- location/src/main/java/com/jvmless/mrsandwich/MrsandwichLocationApplication.java
+++ location/src/main/java/com/jvmless/mrsandwich/MrsandwichLocationApplication.java
@@ -2,8 +2,16 @@ package com.jvmless.mrsandwich;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = {"com.jvmless.mrsandwich.location", "com.jvmless.mrsandwich.location"})
+@EnableDiscoveryClient
+@EnableSwagger2
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.jvmless.mrsandwich.location"})
 public class MrsandwichLocationApplication {
 
     public static void main(String[] args) {
