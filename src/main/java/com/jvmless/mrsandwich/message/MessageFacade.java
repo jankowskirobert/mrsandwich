cab469579527d72fcd6bcb81f2505fced8ec26dc
Robert Jankowski <jankowski.r@hotmail.com>
2019-02-23 23:30:51 +0100

    Creat message to simple crud

--- src/main/java/com/jvmless/mrsandwich/message/MessageFacade.java
+++ src/main/java/com/jvmless/mrsandwich/message/MessageFacade.java
@@ -2,20 +2,27 @@ package com.jvmless.mrsandwich.message;
 
 import com.jvmless.mrsandwich.message.api.CreateMessage;
 import com.jvmless.mrsandwich.notification.VendorId;
+import com.jvmless.mrsandwich.seller.SellerRepository;
+import com.jvmless.mrsandwich.seller.exceptions.SellerNotFoundException;
 
 import java.time.LocalDateTime;
 
 public class MessageFacade {
 
     private final MessageRepository messageRepository;
+    private final SellerRepository sellerRepository;
 
-    public MessageFacade(MessageRepository messageRepository) {
+    public MessageFacade(MessageRepository messageRepository, SellerRepository sellerRepository) {
         this.messageRepository = messageRepository;
+        this.sellerRepository = sellerRepository;
     }
 
     public void addNewMessage(CreateMessage addNewMessage) {
         MessageId messageId = MessageId.of(addNewMessage.getMessageId());
         VendorId vendorId = VendorId.of(addNewMessage.getVendorId());
+        if(!sellerRepository.find(addNewMessage.getVendorId()).isPresent()){
+            throw new SellerNotFoundException("Seller not found");
+        }
         Message newMessage = Message.of(messageId, addNewMessage.getMessageBody(), vendorId, LocalDateTime.now(), addNewMessage.getMessageStatus());
         messageRepository.save(newMessage);
     }
---
+++ src/main/java/com/jvmless/mrsandwich/seller/SpringEventsMqSenderAdapter.java
@@ -0,0 +1,21 @@
+package com.jvmless.mrsandwich.seller;
+
+import com.jvmless.mrsandwich.seller.dto.SellerRegisterDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+
+@Slf4j
+public class SpringEventsMqSenderAdapter implements MQSender {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public SpringEventsMqSenderAdapter(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void newSellerregistrationMessage(SellerRegisterDto sellerDto) {
+        log.info("New Seller Published");
+        applicationEventPublisher.publishEvent(sellerDto);
+    }
+}
